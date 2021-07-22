7a0e967cf0a7a0f4246df8571824044c368ca7b9
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-22 21:09:13 +0530

    Created Node for BinaryImplimentation

---
+++ MyBinaryNode.java
@@ -0,0 +1,16 @@
+package com.bst;
+
+public class MyBinaryNode<T> {
+	int data;
+	MyBinaryNode<T> left;
+	MyBinaryNode<T> right;
+	
+	public MyBinaryNode (int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+		
+	}
+	
+}
+	
